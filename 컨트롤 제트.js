b84af5d8682542b10a1d2b556e04737c5c28fe2e
전한빛 <asjask123@gmail.com>
2023-01-04 23:39:50 +0900

    ✅ 컨트롤 제트

---
+++ 컨트롤 제트.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    let arr = s.split(' ')
+    let result = 0;
+    for(let i=0;i<arr.length;i++){
+       if(arr[i]==='Z' || arr[i+1]==="Z"){
+           result = result 
+       }else {
+           result += Number(arr[i])
+       }
+    }
+    return result
+}
